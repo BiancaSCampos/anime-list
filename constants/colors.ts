9502fea06e3f1f9349362786961a7ed3c39727ee
BiancaSilvaC <biancascampos0410@gmail.com>
2022-02-15 13:46:05 -0300

    Create colors.ts
    This file is to store all of colors that is used in the app

---
+++ constants/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  neutralDark: "#000000",
+  neutralLight: "#ffffff",
+  neutralLight70Opacity: "#D9D9D9",
+  text: "#ffffff",
+  contrast: "#FF1488",
+  midTone: "#403F40",
+};
+
+export default colors;
